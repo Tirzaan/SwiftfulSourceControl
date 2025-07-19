c80412842e1ecdb00085cb090ad488c9c4c25706
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-19 07:07:19 -0500

    [Clean] Added commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Tirzaan on 7/18/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Discription of the feature
+ 
+ BUG IN PRODUCTION: (already to users)
+ [Patch] Discription of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug Fix] Discription of the bug fix
+ 
+ MUNDANE TASKS:
+ [Clean] Discription of the clean
+ 
+ RELEASE:
+ [Release] Discription of the release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
