a5cfca0017b7d94850d1a95d2ae2bf9215b8f38e
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-21 09:00:18 -0500

    [Feature] Fix 1

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Imaginator1!!!")
+                        Text("Imaginator1!")
                         
                         Button("Subscribe now!") {
                         }
