1a1f07e87c3660cefab2e18f384a616661a5c302
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-22 13:43:34 -0500

    [Feature] added profile view

---
+++ SwiftfulSourceControl/ProfileVew.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileVew.swift
+//  SwiftfulSourceControl
+//
+//  Created by Tirzaan on 7/22/25.
+//
+
+import SwiftUI
+
+struct ProfileVew: View {
+    var body: some View {
+        Text("Tirzaan!")
+    }
+}
+
+#Preview {
+    ProfileVew()
+}
